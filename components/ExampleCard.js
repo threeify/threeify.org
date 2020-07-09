85069bb269078498a42d11f3c3b4742b29e51caf
Chris Ryan <theramin108@gmail.com>
2020-07-09 07:55:18 -0700

    update card markup / styling

--- components/ExampleCard.js
+++ components/ExampleCard.js
@@ -1,13 +1,14 @@
-import Link from "next/link";
+export default function ExampleCard(ex) {
+  const { title, description, slug } = ex;
 
-export default function ExampleCard({ title, description, slug }) {
   return (
-    <div className="example-card">
-      <a href={`/examples/${slug}`}>
-        <h3>{title}</h3>
+    <a
+      href={`/examples/${slug}`}
+      style={{ ["--background"]: `url(/assets/thumbnails/${slug}.png)` }}
+      className="example-card"
+    >
+      <h3>{title}</h3>
       <p>{description}</p>
-      <img src={`/assets/thumbnails/${slug}.png`} width="240"></img>
-      </a>
-    </div>
+    </a>
   );
 }
