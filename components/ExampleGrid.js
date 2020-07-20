248cbcc3062d2a9ec0501d952f6743e795453cab
Ben Houston <neuralsoft@gmail.com>
2020-07-20 18:41:24 -0400

    chore: equirectangular fixes.

--- components/ExampleGrid.js
+++ components/ExampleGrid.js
@@ -18,18 +18,12 @@ export default function ExampleGrid(props) {
   for (let i = 0; i < exampleCards.length; i++) {
     currentRow.push(<Col sm={4}>{exampleCards[i]}</Col>);
     if (currentRow.length >= 3) {
-      rows.push(<Row>
-        {currentRow}
-      </Row>);
+      rows.push(<Row>{currentRow}</Row>);
       currentRow = [];
     }
   }
   if (currentRow.length > 0) {
-    rows.push(
-      <Row>
-        {currentRow}
-      </Row>
-    );
+    rows.push(<Row>{currentRow}</Row>);
     currentRow = [];
   }
   return <Container>{rows}</Container>;
